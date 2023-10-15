d644ddaf21235bbbd672096a7b2a83512b03a5a1
Gaurav <Gourav122000@gmail.com>
2023-10-16 00:35:51 +0530

    Done

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import ExpenseForm from "./components/ExpenseForm/ExpenseForm";
+import ExpenseInfo from "./components/ExpenseInfo/ExpenseInfo";
+import ExpenseList from "./components/ExpenseList/ExpenseList";
+import "./App.css";
+
+function App() {
+  const [expenses, setExpenses] = useState([]);
+
+  // Create function to add an expense
+  const addExpense = (expense) => {
+    setExpenses((prevState) => [expense, ...prevState]);
+  };
+
+  // Create function to delete an expense
+  const deleteExpense = (id) => {
+    setExpenses((prevExpenses) => {
+      return prevExpenses.filter((expense) => expense.id !== id);
+    });
+  };
+
+  return (
+    <>
+      <h2 className="mainHeading">Expense Tracker</h2>
+      <div className="App">
+        <ExpenseForm addExpense={addExpense} />
+        <div className="expenseContainer">
+          <ExpenseInfo expenses={expenses} />
+          <ExpenseList expenses={expenses} deleteExpense={deleteExpense} />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
